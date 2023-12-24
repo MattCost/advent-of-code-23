9cece1bd827a1bc8bd212dfec01de6fd2a12d247
Matt <matt.costuros@gmail.com>
2023-12-24 13:52:48 -0500

    this shouldn't be this hard

--- src/day18/Node.cs
+++ src/day18/Node.cs
@@ -24,5 +24,23 @@ public record Node
 {
     public int Row { get; init; }
     public int Col { get; init; }
-    
+
+}
+
+public record Edge
+{
+    public Node Start { get; set; }
+    public Node End { get; set; }
+    public Edge(Node start, Node end)
+    {
+        Start = start;
+        End = end;
+    }
+    public bool IsHorizontal { get { return Start.Row == End.Row; } }
+    public bool IsVertical { get { return Start.Col == End.Col; } }
+
+    public bool ContainsRow(int row)
+    {
+        return  row >= Math.Min(Start.Row, End.Row) && row <= Math.Max(Start.Row, End.Row);
+    }
 }
--- src/day18/Program.cs
+++ src/day18/Program.cs
@@ -66,6 +66,7 @@ catch (Exception e)
 
 long CalculateTrenchVolume(List<Node> nodes)
 {
+   
     var minRow = nodes.Select(node => node.Row).Min();
     var maxRow = nodes.Select(node => node.Row).Max();
     var minCol = nodes.Select(node => node.Row).Min();
@@ -73,67 +74,150 @@ long CalculateTrenchVolume(List<Node> nodes)
     
     Console.WriteLine( new { minRow, maxRow, minCol, maxCol});
     long volume = 0;
-
-    for(int row = minRow-1 ; row <= maxRow ; row++)
+    List<Edge> edges = new();
+    for(int i=0; i<nodes.Count-1 ; i++)
     {
-        for( int col = minCol-1 ; col <= maxCol ; col++)
+        edges.Add( new Edge( nodes[i], nodes[i+1]));
+    }
+    edges.ForEach(Console.WriteLine);
+    for(int row = minRow ; row <= maxRow ; row++)
+    {
+        bool isInside = false;
+        bool onEdge = false;
+        bool upperSet = false;
+        var activeEdges = edges.Where(edge => edge.ContainsRow(row)).ToArray();
+
+        Console.WriteLine($"Row {row} Dealing with {activeEdges.Count()} edges");
+        var currentEdge = activeEdges[0];
+        for(int i=1 ; i<activeEdges.Length ; i++)
         {
-            var isInside = PointInsidePath(row, col, nodes);
-            // Console.WriteLine($"{new {row, col, isInside}}");
-            if(isInside) volume++;
+
+            if(currentEdge.IsVertical && activeEdges[i].IsVertical)
+            {
+                isInside = !isInside;
+                if(isInside && !onEdge)
+                {
+                    volume += Math.Abs(activeEdges[i].Start.Col - currentEdge.Start.Col) - 1;
+                }
+                currentEdge = activeEdges[i];
+                continue;
+
+            }
+            else
+            {
+                var horizontalEdge = currentEdge.IsHorizontal ? currentEdge : activeEdges[i];
+                var verticalEdge = currentEdge.IsVertical ? currentEdge : activeEdges[i];
+                var vertHighest = Math.Min(verticalEdge.Start.Row, verticalEdge.End.Row);
+                if(!onEdge)
+                {
+                    onEdge = true;
+                    if(vertHighest < horizontalEdge.Start.Row)
+                    {
+                        upperSet = true;
+                    }
+                    continue;
+                }
+                else
+                {
+                    onEdge = false;
+                    if( (vertHighest < horizontalEdge.Start.Row) != upperSet)
+                    {
+                        isInside = !isInside;
+                    }
+                    if(i<activeEdges.Length-1)
+                    {
+                        if(isInside)
+                        {
+                            volume += Math.Abs(verticalEdge.End.Col - activeEdges[i+1].Start.Col) ;
+                        }
+                    }
+                }
+            }
         }
     }
+/*
+#######
+#.....#
+###...#
+..#...#
+..#...#
+###.###
+#...#..
+##..###
+.#....#
+.######
 
+proceess each row
+move across row to the right to the next intersection.
+if only 1 edge at this intersection 
+    toggle isInside
+    if(isInside) total += nextIntersection.Col - current.Col
+    continue;
+    
+if 2 edges at horizontal row 
+    if(! onEdge)
+        set onEdge
+        set isAbove if the vertical row is above the hoz row
+    else
+        reset onEdge
+        if iisAbove matches current isAbove - no change - outside row
+        else toggle isInside
+    end if
+
+
+
+
+*/
 
     return volume;
 
 }
 
-bool PointInsidePath(int Py, int Px, List<Node> nodes)
-{
-    // Console.WriteLine($"Entering PointInside Path Row {Py} Col {Px}");
-    var count = 0;
+// bool PointInsidePath(int Py, int Px, List<Node> nodes)
+// {
+//     // Console.WriteLine($"Entering PointInside Path Row {Py} Col {Px}");
+//     var count = 0;
     
-    for(int i=0 ; i<nodes.Count-1 ; i++)
-    {
-        //Edge goes from nodes[i] to nodes[j];
-        var p1x = nodes[i].Col;
-        var p1y = nodes[i].Row;
-        var p2x = nodes[i+1].Col;
-        var p2y = nodes[i+1].Row;
+//     for(int i=0 ; i<nodes.Count-1 ; i++)
+//     {
+//         //Edge goes from nodes[i] to nodes[j];
+//         var p1x = nodes[i].Col;
+//         var p1y = nodes[i].Row;
+//         var p2x = nodes[i+1].Col;
+//         var p2y = nodes[i+1].Row;
 
-        // Console.WriteLine($"\tComparing to edge: ({p1y},{p1x}) to ({p2y},{p2x})");
-        //Corners
-        if( (Py ==p1y ) && (Px == p1x) ) return true;
-        if( (Py ==p2y ) && (Px == p2x) ) return true;
+//         // Console.WriteLine($"\tComparing to edge: ({p1y},{p1x}) to ({p2y},{p2x})");
+//         //Corners
+//         if( (Py ==p1y ) && (Px == p1x) ) return true;
+//         if( (Py ==p2y ) && (Px == p2x) ) return true;
 
-        //Horizontal lines
-        if( (p1y == p2y) &&(Py == p1y) )
-        {
-            if( Px >= Math.Min(p1x, p2x) && Px <= Math.Max(p1x, p2x)) return true;
-        }
-        //Vertical lines
-        if( (p1x == p2x) &&(Px == p1x) )
-        {
-            if( Py >= Math.Min(p1y, p2y) && Py <= Math.Max(p1y, p2y)) return true;
-        }
+//         //Horizontal lines
+//         if( (p1y == p2y) &&(Py == p1y) )
+//         {
+//             if( Px >= Math.Min(p1x, p2x) && Px <= Math.Max(p1x, p2x)) return true;
+//         }
+//         //Vertical lines
+//         if( (p1x == p2x) &&(Px == p1x) )
+//         {
+//             if( Py >= Math.Min(p1y, p2y) && Py <= Math.Max(p1y, p2y)) return true;
+//         }
 
 
-        if( (p1y < Py && p2y < Py) || (p1y >= Py && p2y >= Py))
-        {
-            // Console.WriteLine("\tPoint is above or below edge");
-            continue;
-        }
-        //Cheating cus our lines are always 1d
-        var Sx = Math.Min(p1x, p2x);
-        // Console.WriteLine($"\tIntersection Col {Sx}");
-        if(Sx >= Px)
-        {
-            // Console.WriteLine($"\tIntersection Col >= Col. Increasing Count");
-            count++;
-        }
-    }
+//         if( (p1y < Py && p2y < Py) || (p1y >= Py && p2y >= Py))
+//         {
+//             // Console.WriteLine("\tPoint is above or below edge");
+//             continue;
+//         }
+//         //Cheating cus our lines are always 1d
+//         var Sx = Math.Min(p1x, p2x);
+//         // Console.WriteLine($"\tIntersection Col {Sx}");
+//         if(Sx >= Px)
+//         {
+//             // Console.WriteLine($"\tIntersection Col >= Col. Increasing Count");
+//             count++;
+//         }
+//     }
 
-    // Console.WriteLine($"Exiting PointInside {new {row=Py, col=Px, count}}");
-    return count % 2 == 1;
-}
+//     // Console.WriteLine($"Exiting PointInside {new {row=Py, col=Px, count}}");
+//     return count % 2 == 1;
+// }

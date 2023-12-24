6fcfae710a18cd5434b2508ed2166205a0a93042
Matt <matt.costuros@gmail.com>
2023-12-24 10:19:12 -0500

    garbage

--- src/day18/Node.cs
+++ src/day18/Node.cs
@@ -1,22 +1,28 @@
-public class Edge
-{
-    public Node Node { get; set; }
-    public string Color { get; set; }    
-    public Edge(Node node, string color)
-    {
-        Node = node;
-        Color = color;
-    }
-}
-public class Node
-{
-    public int Row { get; set; }
-    public int Col { get; set; }
-    public Node(int row, int col)
-    {
-        Row = row;
-        Col = col;
-    }
-    public List<Edge> Links { get; set; } = new();
+// public class Edge
+// {
+//     public Node Node { get; set; }
+//     public string Color { get; set; }    
+//     public Edge(Node node, string color)
+//     {
+//         Node = node;
+//         Color = color;
+//     }
+// }
+// public class Node
+// {
+//     public int Row { get; set; }
+//     public int Col { get; set; }
+//     public Node(int row, int col)
+//     {
+//         Row = row;
+//         Col = col;
+//     }
+//     public List<Edge> Links { get; set; } = new();
+// }
 
+public record Node
+{
+    public int Row { get; init; }
+    public int Col { get; init; }
+    
 }
--- src/day18/Program.cs
+++ src/day18/Program.cs
@@ -1,40 +1,48 @@
 ﻿using System.Text;
 
+//62365 -input part 1
 Console.WriteLine("Hello, World!");
 
 string? line;
-Dictionary<string, Node> Nodes = new();
-int row = 0;
-int col = 0;
-var startingNode = new Node(row, col);
-Nodes[GenerateKey(row, col)] = startingNode;
+int row = 1;
+int col=1;
+List<Node> Nodes = new()
+{
+    new Node { Row = row, Col = col }
+};
+
 try
 {
     // StreamReader sr = new StreamReader("sample.txt");
     StreamReader sr = new StreamReader("input.txt");
 
     Console.WriteLine("starting processing");
+
+    long length = 0;
     while (!sr.EndOfStream)
     {
         line = sr.ReadLine();
         while (!string.IsNullOrEmpty(line))
         {
             var split = line.Split(' ');
+
+            //Part 2
+            // var hex = split[2].Trim('(').Trim(')').Trim('#');
+            // var directionCode = hex[5];
+            // var difference = int.Parse(hex.Substring(0, 5), System.Globalization.NumberStyles.HexNumber);
+            
+            // length += difference;
+            // Console.WriteLine($"#{hex} = {directionCode} {difference}");
+
+            //Part 1
             var nextRow = row + (split[0] == "U" ? -int.Parse(split[1]) : split[0] == "D" ? int.Parse(split[1]) : 0);
             var nextCol = col + (split[0] == "L" ? -int.Parse(split[1]) : split[0] == "R" ? int.Parse(split[1]) : 0);
+            length += int.Parse(split[1]);
+            //Part 2
+            // var nextRow = row + directionCode == '3' ? -difference : directionCode == '1' ? difference : 0;
+            // var nextCol = col + directionCode == '2' ? -difference : directionCode == '0' ? difference : 0;
 
-            var currentNode = Nodes[GenerateKey(row, col)];
-            var nextKey = GenerateKey(nextRow, nextCol);
-            if (!Nodes.ContainsKey(nextKey))
-            {
-                Nodes[nextKey] = new Node(nextRow, nextCol);
-            }
-            var nextNode = Nodes[nextKey];
-
-            //add link from row,col to nextRow,nextCol and vice versa
-            currentNode.Links.Add(new Edge(nextNode, split[2].Trim('(').Trim(')')));
-            // nextNode.Links.Add( new Edge(currentNode, split[2].Trim('(').Trim(')')));
-
+            Nodes.Add( new Node{ Row = nextRow, Col = nextCol});
 
             row = nextRow;
             col = nextCol;
@@ -44,157 +52,88 @@ try
     }
     sr.Close();
 
-    int minRow = Nodes.Values.Select(node => node.Row).Min();
-    int minCol = Nodes.Values.Select(node => node.Col).Min();
-    int maxRow = Nodes.Values.Select(node => node.Row).Max();
-    int maxCol = Nodes.Values.Select(node => node.Col).Max();
-
-    Console.WriteLine(new { minRow, minCol, maxRow, maxCol });
-    var rowCount = Math.Abs(minRow) + maxRow + 1;
-    var colCount = Math.Abs(minCol) + maxCol + 1;
-
-    var rowOffset = -minRow;
-    var colOffset = -minCol;
-
-    // char[,] Map = new char[maxRow + 1, maxCol + 1];
-    char[,] Map = new char[rowCount, colCount];
-
-    for (row = 0; row < rowCount; row++)
-    {
-        for (col = 0; col < colCount; col++)
-        {
-            Map[row, col] = '.';
-        }
-    }
-
-    //Draw the # into the map
-    var startingKey = GenerateKeyN(startingNode);
-    var currentNode2 = startingNode;
-    var nextNode2 = currentNode2.Links.First().Node;
-    do
-    {
-        if (currentNode2.Row == nextNode2.Row)
-        {
-            var deltaCol = currentNode2.Col < nextNode2.Col ? 1 : -1;
-            // Console.WriteLine($"Drawing line in map. Row {currentNode2.Row}. Col start {currentNode2.Col} Col end {nextNode2.Col} Delta {deltaCol}");
-            for (int c = currentNode2.Col; c != nextNode2.Col; c += deltaCol)
-            {
-                Map[rowOffset + currentNode2.Row, colOffset + c] = '#';
-            }
-        }
-        else
-        {
-            var deltaRow = currentNode2.Row < nextNode2.Row ? 1 : -1;
-            // Console.WriteLine($"Drawing line in map. Col {currentNode2.Col}. Row start {currentNode2.Row} Row end {nextNode2.Row} Delta {deltaRow}");
-
-            for (int r = currentNode2.Row; r != nextNode2.Row; r += deltaRow)
-            {
-                Map[rowOffset + r, colOffset + currentNode2.Col] = '#';
-            }
-        }
-        currentNode2 = nextNode2;
-        nextNode2 = nextNode2.Links.First().Node;
-    } while (GenerateKeyN(currentNode2) != startingKey);
+    Console.WriteLine("Nodes");
+    Nodes.ForEach( node => Console.WriteLine(node));
 
 
-    // ok we have a graph
-    var length = CalculateTrenchLength(startingNode);
-    var volume = CalculateTrenchVolume(Map, rowCount, colCount);
-    PrintMap(Map, rowCount, colCount);
-    Console.WriteLine(new { length, volume, total = length + volume });
+    var volume = CalculateTrenchVolume(Nodes);
+    Console.WriteLine(new { length, volume});
 }
 catch (Exception e)
 {
     Console.WriteLine("Exception: " + e);
 }
 
-void PrintMap(char[,] map, int rows, int cols)
+long CalculateTrenchVolume(List<Node> nodes)
 {
-    for (int row = 0; row < rows; row++)
+    var minRow = nodes.Select(node => node.Row).Min();
+    var maxRow = nodes.Select(node => node.Row).Max();
+    var minCol = nodes.Select(node => node.Row).Min();
+    var maxCol = nodes.Select(node => node.Row).Max();
+    
+    Console.WriteLine( new { minRow, maxRow, minCol, maxCol});
+    long volume = 0;
+
+    for(int row = minRow-1 ; row <= maxRow ; row++)
     {
-        // var builder = new StringBuilder(cols);
-        for (int col = 0; col < cols; col++)
+        for( int col = minCol-1 ; col <= maxCol ; col++)
         {
-            Console.Write(map[row, col]);
-            // builder[col] = map[row,col];
-        }
-        Console.WriteLine();
-    }
-
-}
-
-long CalculateTrenchVolume(char[,] map, int rows, int cols)
-{
-    long output = 0;
-
-    for (int row = 0; row < rows; row++)
-    {
-        bool isInside = false;
-        bool onEdge = false;
-        bool topSet = false;
-        // bool isEdge = false;
-        for (int col = 0; col < cols - 1; col++)
-        {
-            //If we cross a wall, toggle the tracking bit
-            if (map[row, col] == '#' && map[row, col + 1] == '.' && (col == 0 || map[row, col - 1] == '.' || map[row,col-1]=='*'))
-            {
-                isInside = !isInside;
-                continue;
-            }
-
-            //edge start
-            if ((map[row, col] == '#') && map[row, col + 1] == '#' && !onEdge)
-            {
-                onEdge = true;
-                topSet = row > 0 && map[row - 1, col] == '#';
-                continue;
-            }
-
-            //edge end
-            if(map[row,col] == '#' && onEdge && map[row,col+1] == '.')
-            {
-                if(row != 0 && row != rows-1 && ((map[row-1,col] == '#') != topSet))
-                {
-                    //if the # changed positions, we are on an "inside" edge and need to toggle the tracking bit
-                    isInside = !isInside;
-                }
-                //regardless we reset the onEdge bit
-                onEdge = false;
-            }
-
-            if (map[row, col] == '.' && isInside)
-            {
-                output++;
-                map[row,col] = '*';
-            }
+            var isInside = PointInsidePath(row, col, nodes);
+            // Console.WriteLine($"{new {row, col, isInside}}");
+            if(isInside) volume++;
         }
     }
 
-    return output;
+
+    return volume;
+
 }
 
-string GenerateKey(int row, int col)
+bool PointInsidePath(int Py, int Px, List<Node> nodes)
 {
-    return $"R{row}C{col}";
-}
-
-string GenerateKeyN(Node node)
-{
-    return GenerateKey(node.Row, node.Col);
-}
-
-long CalculateTrenchLength(Node starting)
-{
-    long total = 0;
-    var startingKey = GenerateKeyN(starting);
-    var currentNode = starting;
-    var nextNode = currentNode.Links.First().Node;
-    do
+    // Console.WriteLine($"Entering PointInside Path Row {Py} Col {Px}");
+    var count = 0;
+    
+    for(int i=0 ; i<nodes.Count-1 ; i++)
     {
-        total += Math.Abs(currentNode.Row - nextNode.Row) + Math.Abs(currentNode.Col - nextNode.Col);
-        currentNode = nextNode;
-        nextNode = nextNode.Links.First().Node;
-    } while (GenerateKeyN(currentNode) != startingKey);
-    return total;
+        //Edge goes from nodes[i] to nodes[j];
+        var p1x = nodes[i].Col;
+        var p1y = nodes[i].Row;
+        var p2x = nodes[i+1].Col;
+        var p2y = nodes[i+1].Row;
 
+        // Console.WriteLine($"\tComparing to edge: ({p1y},{p1x}) to ({p2y},{p2x})");
+        //Corners
+        if( (Py ==p1y ) && (Px == p1x) ) return true;
+        if( (Py ==p2y ) && (Px == p2x) ) return true;
+
+        //Horizontal lines
+        if( (p1y == p2y) &&(Py == p1y) )
+        {
+            if( Px >= Math.Min(p1x, p2x) && Px <= Math.Max(p1x, p2x)) return true;
+        }
+        //Vertical lines
+        if( (p1x == p2x) &&(Px == p1x) )
+        {
+            if( Py >= Math.Min(p1y, p2y) && Py <= Math.Max(p1y, p2y)) return true;
+        }
+
+
+        if( (p1y < Py && p2y < Py) || (p1y >= Py && p2y >= Py))
+        {
+            // Console.WriteLine("\tPoint is above or below edge");
+            continue;
+        }
+        //Cheating cus our lines are always 1d
+        var Sx = Math.Min(p1x, p2x);
+        // Console.WriteLine($"\tIntersection Col {Sx}");
+        if(Sx >= Px)
+        {
+            // Console.WriteLine($"\tIntersection Col >= Col. Increasing Count");
+            count++;
+        }
+    }
+
+    // Console.WriteLine($"Exiting PointInside {new {row=Py, col=Px, count}}");
+    return count % 2 == 1;
 }
